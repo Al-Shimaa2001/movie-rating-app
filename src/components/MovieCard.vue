35a409b5e910b0cd29ccccac475e7e6a280c020f
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-08-01 14:32:17 +0300

    replace the simple array with local Api , update the code of structure and add its css styling with tailwind

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,88 +1,25 @@
 <script setup>
-const movies = [
-  {
-    id: 1,
-    name: 'The Shawshank Redemption',
-    genres: ['Drama', 'Crime'],
-    rating: 9.3,
-    poster: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
-    description:
-      'A banker wrongly convicted of murder finds hope and friendship in prison. A tale of perseverance and redemption.',
-    in_theaters: false,
-  },
-  {
-    id: 2,
-    name: 'The Godfather',
-    genres: ['Crime', 'Drama'],
-    rating: 9.2,
-    poster: 'https://image.tmdb.org/t/p/original/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg',
-    description:
-      'A powerful mafia family’s patriarch transfers control to his reluctant son. A saga of power, loyalty, and betrayal.',
-    in_theaters: null,
-  },
-  {
-    id: 3,
-    name: 'The Dark Knight',
-    genres: ['Action', 'Crime', 'Drama'],
-    rating: 9.0,
-    poster: 'https://image.tmdb.org/t/p/original/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
-    description:
-      'Batman faces the chaotic Joker in a battle for Gotham’s soul. A gripping mix of action and psychological depth.',
-    in_theaters: null,
-  },
-  {
-    id: 4,
-    name: 'Pulp Fiction',
-    genres: ['Crime', 'Drama'],
-    rating: 8.9,
-    poster: 'https://image.tmdb.org/t/p/original/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg',
-    description:
-      'Interconnected crime stories unfold with dark humor and violence. A cult classic with unforgettable dialogues.',
-    in_theaters: false,
-  },
-  {
-    id: 5,
-    name: 'Inception',
-    genres: ['Action', 'Adventure', 'Sci-Fi'],
-    rating: 8.8,
-    poster: 'https://image.tmdb.org/t/p/original/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg',
-    description:
-      'A thief enters dreams to plant ideas, but reality blurs. A mind-bending sci-fi thriller by Christopher Nolan.',
-    in_theaters: false,
-  },
-  {
-    id: 6,
-    name: 'Parasite',
-    genres: ['Drama', 'Thriller', 'Comedy'],
-    rating: 8.6,
-    poster: 'https://image.tmdb.org/t/p/original/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg',
-    description:
-      'A poor family schemes to work for a wealthy household. A darkly comic masterpiece on class divide.',
-    in_theaters: false,
-  },
-]
+import movies from '../API/MOCK_DATA.json'
+import RatingComponent from './RatingComponent.vue'
 </script>
 
 <template>
-
-    <main
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3.5 justify-center items-center"
-    >
-      <div v-for="movie in movies" :key="movie.id" class="border text-center">
-        <img :src="movie.poster" :alt="movie.name" class="w-[100px] h-[100px]" />
-        <h3>{{ movie.name }}</h3>
+  <main class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3.5 justify-center m-3">
+    <div v-for="movie in movies" :key="movie.id" class="bg-white text-black rounded-xl m-2">
+      <img :src="movie.poster" :alt="movie.name" class="w-full h-100 rounded-t-xl" />
+      <section class="details py-3 px-5">
+        <h3 class="font-semibold text-2xl mt-3">{{ movie.name }}</h3>
         <span
           v-for="(genres, index) in movie.genres"
           :key="index"
-          class="m-2 bg-purple-500 p-1 rounded-xl"
+          class="inline-block px-1 me-3 bg-blue-400 rounded-2xl text-white"
           >{{ genres }}</span
         >
-        <p>{{ movie.description }}</p>
-        <ul>
-          <li>rating</li>
-        </ul>
-      </div>
-    </main>
+        <p class="my-3 font-semibold text-gray-800">{{ movie.description }}</p>
+        <RatingComponent />
+      </section>
+    </div>
+  </main>
 </template>
 
 <style lang="css" scoped></style>
