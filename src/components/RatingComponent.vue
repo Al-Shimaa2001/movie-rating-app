9758d91af2e1dfc2fcdb000b0c5baf66a1462964
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-08-01 14:33:26 +0300

    but the first structure of rating

--- src/components/RatingComponent.vue
+++ src/components/RatingComponent.vue
@@ -1,13 +1,15 @@
 <script setup>
-
+import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-  <div>
-
+  <div class="flex">
+    <p>Rating (3/5)</p>
+    <ul class="flex">
+      <li><Icon icon="ic:round-star-rate" width="24" height="24" style="color: #faff09" /></li>
+      <li><Icon icon="ic:round-star-rate" width="24" height="24" style="color: #faff09" /></li>
+    </ul>
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
