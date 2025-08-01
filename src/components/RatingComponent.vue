ca8830a495a780a16e1e9386687d0b8e39b840ba
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-08-01 16:30:37 +0300

    feat: implement dynamic rating system with interactive star icons

--- src/components/RatingComponent.vue
+++ src/components/RatingComponent.vue
@@ -1,15 +1,50 @@
 <script setup>
 import { Icon } from '@iconify/vue'
+import { ref } from 'vue'
+const rating = ref([
+  { filled: false },
+  { filled: false },
+  { filled: false },
+  { filled: false },
+  { filled: false },
+])
+
+let rateNumber = ref(0)
+function addRating(index) {
+  if (addEventListener) {
+    rateNumber.value = index + 1
+    rating.value.forEach((star, i) => {
+      star.filled = i <= index
+    })
+  }
+}
 </script>
 
 <template>
   <div class="flex">
-    <p>Rating (3/5)</p>
+    <p>Rating ({{ rateNumber }}/5)</p>
     <ul class="flex">
-      <li><Icon icon="ic:round-star-rate" width="24" height="24" style="color: #faff09" /></li>
-      <li><Icon icon="ic:round-star-rate" width="24" height="24" style="color: #faff09" /></li>
+      <li v-for="(icon, index) in rating" :key="index">
+        <Icon
+          :icon="icon.filled ? 'ic:round-star-rate' : 'material-symbols-light:star-outline-rounded'"
+          width="24"
+          height="24"
+          style="color: #faff09"
+          @click="addRating(index)"
+        />
+      </li>
     </ul>
   </div>
+  <section class="absolute end-1 top-1">
+    <div class="relative">
+      <Icon icon="ic:round-star-rate" width="60" height="60" style="color: #faff09" />
+      <span class="absolute top-4.5 start-6.5 font-semibold text-lg">{{ rateNumber }}</span>
+    </div>
+  </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.clicked {
+  color: #faff09;
+}
+</style>
